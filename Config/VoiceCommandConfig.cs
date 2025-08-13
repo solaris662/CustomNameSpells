01b14be0998fb84f5529cf4c81bae8305777cd36
solaris662 <apolo.parra1@gmail.com>
2025-08-13 16:26:05 -0400

    Update VoiceCommandConfig.cs

--- Config/VoiceCommandConfig.cs
+++ Config/VoiceCommandConfig.cs
@@ -5,56 +5,56 @@ namespace CustomNameSpells.Config
 {
     public static class VoiceCommandConfig
     {
-        public static ConfigEntry<string> FireballCommand;
-        public static ConfigEntry<string> FrostBoltCommand;
-        public static ConfigEntry<string> WormCommand;
-        public static ConfigEntry<string> HoleCommand;
-        public static ConfigEntry<string> MagicMissileCommand;
-        public static ConfigEntry<string> MirrorCommand;
+        public static ConfigEntry<string> bola,fuegoCommand;
+        public static ConfigEntry<string> congelaCommand;
+        public static ConfigEntry<string> entradaCommand;
+        public static ConfigEntry<string> salidaCommand;
+        public static ConfigEntry<string> misil,magicoCommand;
+        public static ConfigEntry<string> espejitoCommand;
         
-        public static ConfigEntry<string> RockCommand;
-        public static ConfigEntry<string> WispCommand;
-        public static ConfigEntry<string> BlastCommand;
-        public static ConfigEntry<string> DivineCommand;
-        public static ConfigEntry<string> BlinkCommand;
-        public static ConfigEntry<string> ThunderboltCommand;
+        public static ConfigEntry<string> RocaCommand;
+        public static ConfigEntry<string> luzCommand;
+        public static ConfigEntry<string> laserCommand;
+        public static ConfigEntry<string> divinoCommand;
+        public static ConfigEntry<string> saltaCommand;
+        public static ConfigEntry<string> rayo,truenoCommand;
 
         public static void Init(ConfigFile config)
         {
-            FireballCommand = config.Bind("Commands", "Fireball", "fire ball fireball", 
-                "Command for Fireball spell (all variants of the word can be separated by a space)");
+            bola,fuegoCommand = config.Bind("Commands", "fuego", "bola", 
+                "Command for bola,fuego spell (all variants of the word can be separated by a space)");
                 
-            FrostBoltCommand = config.Bind("Commands", "FrostBolt", "freeze ease",
+            congelaCommand = config.Bind("Commands", "congela", "hielo",
                 "Command for Frost Bolt spell (all variants of the word can be separated by a space)");
                 
-            WormCommand = config.Bind("Commands", "Worm", "worm",
+            wormCommand = config.Bind("Commands", "entrada", "pueta",
                 "Command for Worm spell (all variants of the word can be separated by a space)");
                 
-            HoleCommand = config.Bind("Commands", "Hole", "hole",
+            HoleCommand = config.Bind("Commands", "salida", "ventana",
                 "Command for Hole spell (all variants of the word can be separated by a space)");
                 
-            MagicMissileCommand = config.Bind("Commands", "MagicMissile", "magic missile",
+            MagicMissileCommand = config.Bind("Commands", "Magico", "magica",
                 "Command for Magic Missile spell (all variants of the word can be separated by a space)");
                 
-            MirrorCommand = config.Bind("Commands", "Mirror", "mirror",
+            MirrorCommand = config.Bind("Commands", "espejito", "mirror",
                 "Command for Mirror spell (all variants of the word can be separated by a space)");
                 
-            RockCommand = config.Bind("AdditionalCommands", "Rock", "rock",
+            RockCommand = config.Bind("AdditionalCommands", "roca", "tierra",
                 "Command for Rock spell (all variants of the word can be separated by a space)");
                 
-            WispCommand = config.Bind("AdditionalCommands", "Wisp", "wisp",
+            WispCommand = config.Bind("AdditionalCommands", "luz", "wisp",
                 "Command for Wisp spell (all variants of the word can be separated by a space)");
                 
-            BlastCommand = config.Bind("AdditionalCommands", "Blast", "dark blast",
+            BlastCommand = config.Bind("AdditionalCommands", "laser", "dark blast",
                 "Command for Blast spell (all variants of the word can be separated by a space)");
                 
-            DivineCommand = config.Bind("AdditionalCommands", "Divine", "divine light",
+            DivineCommand = config.Bind("AdditionalCommands", "divino", "divine light",
                 "Command for Divine spell (all variants of the word can be separated by a space)");
                 
-            BlinkCommand = config.Bind("AdditionalCommands", "Blink", "blink",
+            BlinkCommand = config.Bind("AdditionalCommands", "saltar", "blink",
                 "Command for Blink spell (all variants of the word can be separated by a space)");
                 
-            ThunderboltCommand = config.Bind("AdditionalCommands", "Thunderbolt", "thunderbolt thunder bolt",
+            ThunderboltCommand = config.Bind("AdditionalCommands", "rayo", "trueno",
                 "Command for Thunderbolt spell (all variants of the word can be separated by a space)");
         }
     }
